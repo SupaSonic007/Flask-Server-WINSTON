0154c1c502ccb578e4a10cfbf07006a4df45ae8c
Joshua Kisnorbo <joshua.kisnorbo@stu.burgmann.act.edu.au>
2023-05-11 11:38:53 +1000

    Fix small issues

--- app/routes.py
+++ app/routes.py
@@ -4,20 +4,20 @@ from flask import render_template, flash, url_for, request
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Home - W.I.N.S.T.O.N.')
+    return render_template('index.html', title='Home')
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title='About - W.I.N.S.T.O.N.')
+    return render_template('about.html', title='About')
 
 @app.route('/post')
 @app.route('/posts')
 def posts():
-    return render_template('posts.html', title='Winstogram - W.I.N.S.T.O.N.')
+    return render_template('posts.html', title='Winstogram')
 
 @app.route('/stream')
 def stream():
-    return render_template('stream.html', title='Stream - W.I.N.S.T.O.N.')
+    return render_template('stream.html', title='Stream')
 
 @app.route('/user/<username>')
 def user(username):
@@ -25,4 +25,4 @@ def user(username):
     # Database not setup yet but will use a query
     user = None 
 
-    return render_template('user.html', title=f'{username} - W.I.N.S.T.O.N.', user=user)
+    return render_template('user.html', title=f'{username}', user=user)
