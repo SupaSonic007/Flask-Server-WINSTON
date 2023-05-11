d1d1242584a671a2b2cadefcd66b3a37fa16fd52
Joshua Kisnorbo <joshua.kisnorbo@stu.burgmann.act.edu.au>
2023-05-11 12:41:28 +1000

    Implement post visual

--- app/routes.py
+++ app/routes.py
@@ -12,8 +12,35 @@ def about():
 
 @app.route('/post')
 @app.route('/posts')
-def posts():
-    return render_template('posts.html', title='Winstogram')
+def winstogram():
+    posts = [
+        {
+            'header': 'Epic',
+            'body': 'Also epic',
+            'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/182px-Python-logo-notext.svg.png'
+        },
+        {
+            'header': 'Epic 2',
+            'body': 'Also epic'
+        },
+        {
+            'header': 'Epic',
+            'body': 'Also epic'
+        },
+        {
+            'header': 'Epic 2',
+            'body': 'Also epic'
+        },
+        {
+            'header': 'Epic',
+            'body': 'Also epic'
+        },
+        {
+            'header': 'Epic 2',
+            'body': 'Also epic'
+        },
+    ]
+    return render_template('posts.html', title='Winstogram', posts=posts)
 
 @app.route('/stream')
 def stream():
