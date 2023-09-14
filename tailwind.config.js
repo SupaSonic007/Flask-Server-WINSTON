f3146f366fa503d0e9608a4de10cf29941255a53
joshey007 <joshua.kisnorbo@stu.burgmann.act.edu.au>
2023-09-14 12:02:45 +1000

    Fix list styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './app/templates/*.{html, js}'
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary:'#f3eef3',
-        secondary: '#423E3B',
-        accent: '#ffffff',
-        lightSecondary: '#4f4b49',
-        tertiary: '#f9f9f9',
-        // primary:'#fffaff',
-        // secondary: '#423E3B',
-        // accent: '#ffffff',
-      }
+    content: ['./app/templates/*.{html, js}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#f3eef3',
+                secondary: '#423E3B',
+                accent: '#ffffff',
+                lightSecondary: '#4f4b49',
+                tertiary: '#f9f9f9',
+            },
+        },
     },
-    },
-    
-  plugins: [
-    require('@tailwindcss/typography'),
-  ]
-}
+
+    plugins: [require('@tailwindcss/typography')],
+};
